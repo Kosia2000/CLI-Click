dfb85e8813009b0592b0ca72bbb177e368ce0132
Kosia2000 <kosia.babiuch@gmail.com>
2020-12-18 21:58:38 +0100

    Add helpers to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '2.0.0',
     python_requires = '==3.6.9',
     url='https://github.com/Kosia2000/CLI-Click',
-    py_modules = ['click_app'],
+    py_modules = ['click_app', 'helpers'],
     install_requires=[
         'click==7.1.2',
         'termcolor == 1.1.0',
